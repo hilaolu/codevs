411eb11a4ce81d01c32c06b4f96963b90ba1eaa9
hilaolu <i@hilaolu.com>
2017-11-21 19:35:31 +0800

    更新 'codevs/1576.cpp'

--- codevs/1576.cpp
+++ codevs/1576.cpp
@@ -1,17 +1,34 @@
 #include<iostream>
-#include<stdio.h>
+#include<cstdio>
 using namespace std;
+int a[101];
+int c[101];
+int find(int len,int k){
+	int mid,left=1,right=len;
+	while(left<=right){
+		mid=(left+right)/2;
+		if(c[mid]==k)return mid;
+		if(c[mid]>k){right=mid-1;continue;}
+		if(c[mid]<k){left=mid+1;continue;}
+	}
+	return mid;
+}
 int main(){
-	int s[5000],a[5000],n;
+	int n,len=1,p;
 	scanf("%d",&n);
-	for(int i=0;i<n;i++)scanf("%d",&s[i]);
-	for(int i=0;i<n;i++)a[i]=1;
+	for(int i=0;i<n;i++)scanf("%d",&a[i]);
+	c[1]=a[0];
 	for(int i=1;i<n;i++){
-		for(int j=i-1;j>=0;j--){
-			if(s[j]<s[i])a[i]=(a[j]+1>a[i])?a[j]+1:a[i];
+		if(a[i]>c[len]){
+			len++;
+			c[len]=a[i];
+		}else{
+			p=find(len,a[i]);
+			c[p]=a[i];
 		}
 	}
-	cout<<a[n-1];
+	cout<<len;
 	return 0;
 }
+
 	
